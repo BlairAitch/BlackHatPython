7f0762c7b9c2158f19dea6115237c8a983d9cd80
BlairAitch <65369891+BlairAitch@users.noreply.github.com>
2022-02-12 14:20:46 +0000

    Finishing TCP Server

--- Exercises/SimpleTCPServer.py
+++ Exercises/SimpleTCPServer.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 bind_ip = "0.0.0.0"
-bind_port = 9999
+bind_port = 22335
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,3 +16,21 @@ def handle_client(client_socket):
 
     # print out what the client sends
     request = client_socket.recv(1024)
+
+    print "[*] Received: %s" % request
+
+    # send back a packet
+    client_socket.send("ACK!")
+
+    client_socket.close()
+
+while True:
+
+    client, addr = server.accept()
+
+    print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+
+    # spin up our client thread to handle incoming data
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
+
